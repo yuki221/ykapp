013fdd4750510acbda838ab4042ff49f9fb6314b
chris <e1521411@gmail.com>
2020-04-08 02:20:33 +0900

    [update]かんたんログインの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 
+  def self.guest
+    find_or_create_by(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.confirmed_at = Time.now
+    end
+  end
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
