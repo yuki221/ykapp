59dee8e652f2f133bd9a7d1b332bb3781c4c2d9e
chris <e1521411@gmail.com>
2020-04-24 15:10:22 +0900

    [upfate]deviseの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address => "smtp.gmail.com", # smtpサーバーのホスト名
+   :port => 587,
+   :authentication => :plain,
+   :user_name => "e1521411@gmail.com",
+   :password => "dqezfnzxjxpebnjd"
+   enable_starttls_auto: true
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.serve_static_files = true
 end
