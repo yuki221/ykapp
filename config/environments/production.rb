81eeb17d31bbafc4cca3de66e3f212416bd5f9c0
chris <e1521411@gmail.com>
2020-04-24 16:24:55 +0900

    [fix]メール設定内ホストの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: 'example.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
