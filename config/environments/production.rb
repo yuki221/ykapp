25b44f199d19b0fa1b8cfda45b2e04a1a13ca759
chris <e1521411@gmail.com>
2020-05-19 15:53:01 +0900

    [fix]ホストの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ykapp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'koispo.net' }
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
