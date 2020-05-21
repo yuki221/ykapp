61f07ff1f866d1676ddf83070cb5347473d44048
chris <e1521411@gmail.com>
2020-05-21 09:09:34 +0900

    [fix]デザインの修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def destroy
+    super
+  end
 
   def new_guest
     user = User.guest
@@ -32,5 +25,12 @@ class Users::SessionsController < Devise::SessionsController
   rescue ActiveRecord::RecordInvalid => e
     pp e.record&.errors
     redirect_to root_path, alert: 'エラーが発生しました。'
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   end
 end
